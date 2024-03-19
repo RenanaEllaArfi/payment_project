f0274664b236fce149b8b1c205d5633b5c465b6f
RenanaEllaArfi <164000920+RenanaEllaArfi@users.noreply.github.com>
2024-03-19 12:31:41 +0200

    Create sql.connection.ts

---
+++ src/db/sql/sql.connection.ts
@@ -0,0 +1,14 @@
+import mysql from "mysql2/promise";
+import fs from 'fs';
+
+process.env = JSON.parse(fs.readFileSync('./app-config.json', 'utf8'));
+
+const { SQL_HOST, SQL_PORT, SQL_USER, SQL_PASSWORD, DB_NAME } = process.env;
+
+export const sql_con = await (async () => await mysql.createConnection({
+    host: SQL_HOST,
+    port: Number(SQL_PORT),
+    user: SQL_USER,
+    password: SQL_PASSWORD,
+    database: DB_NAME
+}))();
